5cfcb2572f7d014efa5077157367d58681f2948e
liguofu <464616365@qq.com>
2016-11-09 17:43:07 +0800

    规范代码样式.

--- appcan-ios/webkitCorePalm/Classes/engine/universalex/EUExBase.m
+++ appcan-ios/webkitCorePalm/Classes/engine/universalex/EUExBase.m
@@ -58,18 +58,27 @@
 - (void)clean{
 }
 
--(NSString*)absPath:(NSString*)inPath{
-    ACENSLog(@"inpath start=%@",inPath);
-    NSLog(@"appcan-->EUExBase.m-->absPath-->inpath start=%@",inPath);
-
+- (NSString*)absPath:(NSString*)inPath {
+    
     inPath = [inPath stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    
     if ([inPath hasPrefix:@"file://"]) {
+        
         inPath = [inPath substringFromIndex:[@"file://" length]];
+        
         return inPath;
     }
-    if ([inPath hasPrefix:@"/var/mobile"]||[inPath hasPrefix:@"assets-library"]||[inPath hasPrefix:@"/private/var/mobile"]||[inPath hasPrefix:@"/Users"]||[inPath hasPrefix:@"file://"]) {
+    
+    if ([inPath hasPrefix:@"/var/mobile"]||
+        [inPath hasPrefix:@"assets-library"]||
+        [inPath hasPrefix:@"/private/var/mobile"]||
+        [inPath hasPrefix:@"/Users"]||
+        [inPath hasPrefix:@"file://"]) {
+        
         return inPath;
+        
     }
+    
     NSURL *curURL = [self.meBrwView curUrl];
     
     NSString *scheme = [[NSURL URLWithString:inPath] scheme];
@@ -77,61 +86,85 @@
     inPath = [BUtility makeUrl:[curURL absoluteString] url:inPath];
     
     //box://
-    NSLog(@"appcan-->EUExBase.m-->absPath-->%@",inPath);
-
-   if ([inPath hasPrefix:F_BOX_PATH] || [inPath hasPrefix:F_EXTERBOX_PATH]) {
+    if ([inPath hasPrefix:F_BOX_PATH] || [inPath hasPrefix:F_EXTERBOX_PATH]) {
+        
         NSString * str = [BUtility getDocumentsPath:@"box"];
+        
         if (![[NSFileManager defaultManager] fileExistsAtPath:str]) {
+            
             [[NSFileManager defaultManager] createDirectoryAtPath:str withIntermediateDirectories:YES attributes:nil error:nil];
         }
-       
-         NSString *resultStr = [NSString stringWithFormat:@"%@/%@",str,[inPath substringFromIndex:scheme.length+3]];;
-       
-        //NSLog(@"str=%@",resultStr);
+        
+        NSString *resultStr = [NSString stringWithFormat:@"%@/%@",str,[inPath substringFromIndex:scheme.length+3]];;
+        
         return resultStr;
     }
+    
     if ([inPath hasPrefix:F_WGTS_PATH]||[inPath hasPrefix:F_APP_PATH]||[inPath hasPrefix:F_RES_PATH]) {
-        //		EBrowserWindowContainer *eBrwWndContainer = (EBrowserWindowContainer*)meBrwView.meBrwWnd.superview;
         
+        //EBrowserWindowContainer *eBrwWndContainer = (EBrowserWindowContainer*)meBrwView.meBrwWnd.superview;
         EBrowserWindowContainer *eBrwWndContainer = [EBrowserWindowContainer getBrowserWindowContaier:self.meBrwView];
         NSString *absPath =nil;
         absPath=[self.meBrwView.meBrwCtrler.mwWgtMgr curWidgetPath:eBrwWndContainer.mwWgt];
-        ACENSLog(@"abspath=%@",absPath);
         NSString *relativePath=nil;
+        
         if ([inPath hasPrefix:F_APP_PATH]) {
+            
             relativePath =[inPath substringFromIndex:6];
         }
+        
         if ([inPath hasPrefix:F_RES_PATH]) {
+            
             relativePath =[inPath substringFromIndex:6];
+            
             if (eBrwWndContainer.mwWgt.wgtType==F_WWIDGET_MAINWIDGET) {
+                
                 BOOL isCopyFinish = [[[NSUserDefaults standardUserDefaults]objectForKey:F_UD_WgtCopyFinish] boolValue];
+                
                 if (theApp.useUpdateWgtHtmlControl && isCopyFinish) {
+                    
                     if ([BUtility getSDKVersion]<5.0) {
+                        
                         absPath =[BUtility getCachePath:@"widget/wgtRes"];
-                    }else {
+                        
+                    } else {
+                        
                         absPath =[BUtility getDocumentsPath:@"widget/wgtRes"];
                     }
-                }else {
+                    
+                } else {
+                    
                     absPath =[BUtility getResPath:@"widget/wgtRes"];
+                    
                 }
-            }else {
+                
+            } else {
+                
                 absPath = [NSString stringWithFormat:@"%@/wgtRes",absPath];
+                
             }
-            ACENSLog(@"absPath middle=%@",absPath);
             
         }
+        
         if ([inPath hasPrefix:F_WGTS_PATH]) {
+            
             absPath = [BUtility getDocumentsPath:@"widgets"];
-            relativePath =[inPath substringFromIndex:7]; 
+            
+            relativePath =[inPath substringFromIndex:7];
         }
+        
         inPath = [NSString stringWithFormat:@"%@/%@",absPath,relativePath];
-        ACENSLog(@"inPath end=%@",inPath);
     }
+    
     if ([inPath hasPrefix:@"file://"]) {
+        
         inPath = [inPath substringFromIndex:[@"file://" length]];
+        
     }
+    
     return inPath;
 }
+
 @end
 
 
